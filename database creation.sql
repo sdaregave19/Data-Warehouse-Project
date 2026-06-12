769eb5b760037387e1472ad9a1de75b270ba28eb
Sangamesh Daregave <sangameshdaregave19@gmail.com>
2026-06-12 13:51:05 +0530

    database creation

---
+++ database creation.sql
@@ -0,0 +1,7 @@
+DROP DATABASE if EXISTS DataWarehouse;
+CREATE DATABASE Datawarehouse;
+use DataWarehouse;
+SELECT DATABASE();
+
+
+-- SCHEMA->A database contains one or more schemas, and a schema contains tables and other database objects.
