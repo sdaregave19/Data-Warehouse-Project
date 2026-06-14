bd7ca67fcac186d217a9174f58819123d81eb61e
Sangamesh Daregave <sangameshdaregave19@gmail.com>
2026-06-14 16:03:47 +0530

    day 3

--- Scripts/bronze/proc_load_bronze.sql
+++ Scripts/bronze/proc_load_bronze.sql
@@ -1,5 +1,60 @@
-load data local infile 'E:\code\PROJECTS\Data-Warehouse-Project\Dataset\source_crm\cust_info.csv'
-into table crm_cust_info
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
+-- BULK INSERT
+USE DataWarehouse;
+
+TRUNCATE TABLE bronze.crm_cust_info
+
+BULK INSERT bronze.crm_cust_info
+FROM 'E:\code\PROJECTS\Data-Warehouse-Project\Dataset\source_crm\cust_info.csv'
+WITH (
+    firstrow=2,
+    FIELDTERMINATOR=',',
+    TABLOCK
+)
+
+SELECT  COUNT(*) FROM bronze.crm_cust_info;
+
+-- ! not loading
+BULK INSERT bronze.crm_prd_info
+FROM 'E:\code\PROJECTS\Data-Warehouse-Project\Dataset\source_crm\prd_info'
+WITH (
+    firstrow=2,
+    FIELDTERMINATOR=',',
+    TABLOCK
+)
+
+-- ! not loading
+BULK INSERT bronze.crm_sales_details
+FROM 'E:\code\PROJECTS\Data-Warehouse-Project\Dataset\source_crm\sales_details'
+WITH (
+    firstrow=2,
+    FIELDTERMINATOR=',',
+    TABLOCK
+)
+
+
+BULK INSERT bronze.erp_cust_az12
+FROM 'E:\code\PROJECTS\Data-Warehouse-Project\Dataset\source_erp\cust_az12.csv'
+WITH (
+    firstrow=2,
+    FIELDTERMINATOR=',',
+    TABLOCK
+)
+
+
+BULK INSERT bronze.erp_loc_a101
+FROM 'E:\code\PROJECTS\Data-Warehouse-Project\Dataset\source_erp\loc_a101.csv'
+WITH (
+    firstrow=2,
+    FIELDTERMINATOR=',',
+    TABLOCK
+)
+
+BULK INSERT bronze.erp_px_cat_g1v2
+FROM 'E:\code\PROJECTS\Data-Warehouse-Project\Dataset\source_erp\PX_CAT_G1V2.csv'
+WITH (
+    firstrow=2,
+    FIELDTERMINATOR=',',
+    TABLOCK
+)
+
+SELECT  * FROM bronze.erp_px_cat_g1v2;
--- Scripts/database_creation.sql
+++ Scripts/database_creation.sql
@@ -1,4 +1,26 @@
-SELECT * FROM sys.databases;
+-- !Data Warehouse project, names like bronze, silver, and gold are often used as separate databases/layers
+-- SCHEMA->A database contains one or more schemas, and a schema contains tables and other database objects.
 
-USE TRAIL;
 
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+
+
+USE DataWarehouse;
+
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+
+
+CREATE SCHEMA silver;
+
+
+CREATE SCHEMA gold;
