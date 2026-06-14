ddf32bc2afe1915551ed1e736158fe8529a7df30
Sangamesh Daregave <sangameshdaregave19@gmail.com>
2026-06-14 22:13:00 +0530

    3

--- Scripts/bronze/proc_load_bronze.sql
+++ Scripts/bronze/proc_load_bronze.sql
@@ -1,60 +1,122 @@
 -- BULK INSERT
-USE DataWarehouse;
-
-TRUNCATE TABLE bronze.crm_cust_info
-
-BULK INSERT bronze.crm_cust_info
-FROM 'E:\code\PROJECTS\Data-Warehouse-Project\Dataset\source_crm\cust_info.csv'
-WITH (
-    firstrow=2,
-    FIELDTERMINATOR=',',
-    TABLOCK
-)
-
-SELECT  COUNT(*) FROM bronze.crm_cust_info;
-
--- ! not loading
-BULK INSERT bronze.crm_prd_info
-FROM 'E:\code\PROJECTS\Data-Warehouse-Project\Dataset\source_crm\prd_info'
-WITH (
-    firstrow=2,
-    FIELDTERMINATOR=',',
-    TABLOCK
-)
-
--- ! not loading
-BULK INSERT bronze.crm_sales_details
-FROM 'E:\code\PROJECTS\Data-Warehouse-Project\Dataset\source_crm\sales_details'
-WITH (
-    firstrow=2,
-    FIELDTERMINATOR=',',
-    TABLOCK
-)
+-- USE DataWarehouse;
 
 
-BULK INSERT bronze.erp_cust_az12
-FROM 'E:\code\PROJECTS\Data-Warehouse-Project\Dataset\source_erp\cust_az12.csv'
-WITH (
-    firstrow=2,
-    FIELDTERMINATOR=',',
-    TABLOCK
-)
+CREATE OR ALTER PROCEDURE bronze.load_bronze 
+AS
+BEGIN
+    DECLARE @start_time DATETIME , @end_time DATETIME , @batch_start DATETIME , @batch_end DATETIME;
+    BEGIN TRY
+        SET @batch_start=GETDATE();
+        PRINT'********************************************************'
+        PRINT 'Loading Bronze Layer'
+        PRINT'********************************************************'
+
+        PRINT'--------------------------------------------------------'
+        PRINT'Loading CRM Tables';
+
+        SET @start_time=GETDATE();
+        TRUNCATE TABLE bronze.crm_cust_info;
+        BULK INSERT bronze.crm_cust_info
+        FROM 'E:\code\PROJECTS\Data-Warehouse-Project\Dataset\source_crm\cust_info.csv'
+        WITH (
+            firstrow=2,
+            FIELDTERMINATOR=',',
+            TABLOCK
+        );
+        SET @end_time=GETDATE();
+        PRINT'>>> Load Duration: '+CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR)+'seconds';
+        PRINT'////////////////////';
+
+        SET @start_time=GETDATE();
+        TRUNCATE TABLE bronze.crm_prd_info;
+        BULK INSERT bronze.crm_prd_info 
+        FROM 'E:\code\PROJECTS\Data-Warehouse-Project\Dataset\source_crm\prd_info.csv'
+        WITH (
+            firstrow=2,
+            FIELDTERMINATOR=',',
+            TABLOCK
+        );
+        SET @end_time=GETDATE();
+        PRINT'>>> Load Duration: '+CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR)+'seconds';
+        PRINT'////////////////////';
+
+        SET @start_time=GETDATE();
+        TRUNCATE TABLE bronze.crm_sales_details;
+        BULK INSERT bronze.crm_sales_details
+        FROM 'E:\code\PROJECTS\Data-Warehouse-Project\Dataset\source_crm\sales_details.csv'
+        WITH (
+            firstrow=2,
+            FIELDTERMINATOR=',',
+            TABLOCK
+        );
+        SET @end_time=GETDATE();
+        PRINT'>>> Load Duration: '+CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR)+'seconds';
+        
+        PRINT'--------------------------------------------------------'
+
+        PRINT'--------------------------------------------------------'
+        PRINT'Loading ERP Tables'
+        PRINT'--------------------------------------------------------'
+
+        SET @start_time=GETDATE();
+        TRUNCATE TABLE bronze.erp_cust_az12;
+        BULK INSERT bronze.erp_cust_az12
+        FROM 'E:\code\PROJECTS\Data-Warehouse-Project\Dataset\source_erp\cust_az12.csv'
+        WITH (
+            firstrow=2,
+            FIELDTERMINATOR=',',
+            TABLOCK
+        );
+        SET @end_time=GETDATE();
+        PRINT'>>> Load Duration: '+CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR)+'seconds';
+        PRINT'////////////////////';
+
+        SET @start_time=GETDATE();
+        TRUNCATE TABLE bronze.erp_loc_a101;
+        BULK INSERT bronze.erp_loc_a101
+        FROM 'E:\code\PROJECTS\Data-Warehouse-Project\Dataset\source_erp\loc_a101.csv'
+        WITH (
+            firstrow=2,
+            FIELDTERMINATOR=',',
+            TABLOCK
+        );
+        SET @end_time=GETDATE();
+        PRINT'>>> Load Duration: '+CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR)+'seconds';
+        PRINT'////////////////////';
 
 
-BULK INSERT bronze.erp_loc_a101
-FROM 'E:\code\PROJECTS\Data-Warehouse-Project\Dataset\source_erp\loc_a101.csv'
-WITH (
-    firstrow=2,
-    FIELDTERMINATOR=',',
-    TABLOCK
-)
+        SET @start_time=GETDATE();
+        TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+        BULK INSERT bronze.erp_px_cat_g1v2
+        FROM 'E:\code\PROJECTS\Data-Warehouse-Project\Dataset\source_erp\PX_CAT_G1V2.csv'
+        WITH (
+            firstrow=2,
+            FIELDTERMINATOR=',',
+            TABLOCK
+        );
+        SET @end_time=GETDATE();
+        PRINT'>>> Load Duration: '+CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR)+'seconds';
+        PRINT'////////////////////';
 
-BULK INSERT bronze.erp_px_cat_g1v2
-FROM 'E:\code\PROJECTS\Data-Warehouse-Project\Dataset\source_erp\PX_CAT_G1V2.csv'
-WITH (
-    firstrow=2,
-    FIELDTERMINATOR=',',
-    TABLOCK
-)
+        --Bronze layer duration
+        SET @batch_end=GETDATE();
+        PRINT' ';
+        PRINT'$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
+        PRINT'Loading bronze layer completed';
+        PRINT'Total Duration : '+CAST(DATEDIFF(second,@batch_start,@batch_end) AS NVARCHAR)+'seconds';
+        PRINT'$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
+        PRINT' '
+        END TRY
+        BEGIN CATCH
+        PRINT'-------------------------------------------'
+        PRINT'ERROR OCCURED DURING LOADING BRONZE LAYER'
+        PRINT'Error Message : '+ERROR_MESSAGE();
+        PRINT'Error Number : '+CAST(ERROR_NUMBER() AS NVARCHAR); --?use CAST() because PRINT can only print a string,
+        PRINT'-------------------------------------------'
+        END CATCH
+END
 
-SELECT  * FROM bronze.erp_px_cat_g1v2;
+
+
+EXEC bronze.load_bronze;
