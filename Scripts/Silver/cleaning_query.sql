a9a023fd907507c366a8db69fe58d66d3b329072
Sangamesh Daregave <sangameshdaregave19@gmail.com>
2026-06-20 16:49:34 +0530

    7

--- Scripts/Silver/cleaning_query.sql
+++ Scripts/Silver/cleaning_query.sql
@@ -282,3 +282,114 @@ FROM bronze.crm_sales_details
 WHERE sls_cust_id NOT IN (SELECT cst_id FROM silver.crm_cust_info)
 
 -- ? Check for invalid dates
+
+SELECT 
+NULLIF(sls_order_dt,0) sls_order_dt                              
+FROM bronze.crm_sales_details
+WHERE sls_order_dt <=0 OR LEN(sls_order_dt)!= 8
+
+SELECT
+    sls_ord_num     ,
+    sls_prd_key     ,
+    sls_cust_id     ,
+    CASE 
+        WHEN sls_order_dt = 0 OR LEN(sls_order_dt)!=8 THEN NULL  
+        ELSE  CAST(CAST(sls_order_dt AS varchar) as date)
+    END as sls_order_dt,
+    CASE 
+        WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt)!=8 THEN NULL  
+        ELSE  CAST(CAST(sls_ship_dt AS varchar) as date)
+    END as sls_ship_dt,
+    CASE 
+        WHEN sls_due_dt = 0 OR LEN(sls_due_dt)!=8 THEN NULL  
+        ELSE  CAST(CAST(sls_due_dt AS varchar) as date)
+    END as sls_due_dt,
+    sls_sales       ,
+    sls_quantity    ,
+    sls_price       
+from bronze.crm_sales_details
+
+
+-- ? Check for invalid dates order
+SELECT * FROM bronze.crm_sales_details
+WHERE sls_order_dt>sls_ship_dt OR sls_order_dt>sls_due_dt
+
+
+/* bsiness rule :-
+sales = quantity * price
+-ve ,0 , null are not allowed
+*/
+
+SELECT DISTINCT
+sls_sales AS old_sls_sales,
+sls_quantity,
+sls_price AS old_sls_price,
+CASE 
+    WHEN sls_sales is NULL or sls_sales <= 0 or sls_sales != sls_quantity* ABS(sls_price)  THEN  sls_quantity * ABS(sls_price)
+    ELSE  sls_sales
+END as sls_sales,
+
+CASE 
+    WHEN sls_price is NULL or sls_price <= 0 THEN  sls_sales / NULLIF(sls_quantity,0)
+    ELSE  sls_price
+END as sls_price
+from bronze.crm_sales_details
+WHERE sls_sales != sls_quantity*sls_price
+or sls_sales is NULL or sls_quantity <=0 or sls_price<=0
+ORDER BY sls_sales , sls_quantity,sls_price  -- sales and price is not proper
+
+-- combine to main query
+INSERT into silver.crm_sales_details(
+    sls_ord_num     ,
+    sls_prd_key     ,
+    sls_cust_id     ,
+    sls_order_dt    ,
+    sls_ship_dt     ,
+    sls_due_dt      ,
+    sls_sales       ,
+    sls_quantity    ,
+    sls_price 
+)
+SELECT
+    sls_ord_num     ,
+    sls_prd_key     ,
+    sls_cust_id     ,
+    CASE 
+        WHEN sls_order_dt = 0 OR LEN(sls_order_dt)!=8 THEN NULL  
+        ELSE  CAST(CAST(sls_order_dt AS varchar) as date)
+    END as sls_order_dt,
+    CASE 
+        WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt)!=8 THEN NULL  
+        ELSE  CAST(CAST(sls_ship_dt AS varchar) as date)
+    END as sls_ship_dt,
+    CASE 
+        WHEN sls_due_dt = 0 OR LEN(sls_due_dt)!=8 THEN NULL  
+        ELSE  CAST(CAST(sls_due_dt AS varchar) as date)
+    END as sls_due_dt,
+    CASE 
+        WHEN sls_sales is NULL or sls_sales <= 0 or sls_sales != sls_quantity* ABS(sls_price)  THEN  sls_quantity * ABS(sls_price)
+        ELSE  sls_sales
+    END as sls_sales,
+    sls_quantity    ,
+    CASE 
+        WHEN sls_price is NULL or sls_price <= 0 THEN  sls_sales / NULLIF(sls_quantity,0)
+        ELSE  sls_price
+    END as sls_price
+    
+from bronze.crm_sales_details
+
+SELECT * FROM silver.crm_sales_details
+
+
+-- !  ****************************************************************
+-- ! table->erp_cust_az12
+
+SELECT 
+CASE 
+    WHEN cid like 'NAS%' THEN  SUBSTRING(cid,4,LEN(cid))
+    ELSE  cid
+END  ascid,
+bdate,
+gen
+from bronze.erp_cust_az12
+
